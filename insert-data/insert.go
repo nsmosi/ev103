b60a6a43f3baf198d20f922185aa302922cc323f
Sardar <sardar.andimeh7@gmail.com>
2024-07-16 18:44:06 +0200

    feat: insert data into redis

--- insert-data/insert.go
+++ insert-data/insert.go
@@ -1,7 +1,40 @@
 package insertdata
 
-func SimCards() error {
+import (
+	"fmt"
+	"log"
 
+	"github.com/SardarAndimeh/ev101/db"
+)
+
+func AddSimCards(filePath string) error {
+
+	records, err := LoadCSV(filePath)
+	if err != nil {
+		log.Fatalf("could not Load CSV data: %v", err)
+	}
+
+	headers := []string{"MSISDN", "IMSI", "ICCID", "Secret", "TAC", "EID", "CID", "IMEI", "BundleID"}
+
+	// Loop through CSV data (sim cards data)
+	for _, record := range records {
+
+		msisdn := record[0]
+		key := "msisdn:" + msisdn
+
+		// create a map of key and values for each Sim card record ->  "MSISDN": 811502214250 , "IMSI": 217500013105250 ,....
+		recordMap := make(map[string]interface{})
+		for index, header := range headers {
+			if index < len(record) {
+				recordMap[header] = record[index]
+			}
+		}
+
+		//insert vreated Map into database
+		err = db.Clients[0].HSet(db.Ctx, key, recordMap).Err()
+		if err != nil {
+			return fmt.Errorf("inserting data into db (HSet) failed %w", err)
+		}
+	}
 	return nil
-
 }
