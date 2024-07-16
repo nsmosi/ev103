a8f3ff8408f569e31c8bac6636a2c9006550b65a
Sardar <sardar.andimeh7@gmail.com>
2024-07-16 18:35:54 +0200

    open file action

--- insert-data/load.go
+++ insert-data/load.go
@@ -1,6 +1,17 @@
 package insertdata
 
-func LoadCSV() ([][]string, error) {
+import (
+	"fmt"
+	"os"
+)
+
+func LoadCSV(filePath string) ([][]string, error) {
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("could not open file %w", err)
+	}
+	defer file.Close()
 
 	return nil, nil
 }
