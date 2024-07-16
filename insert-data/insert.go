eee4527f13b6978390e015c8b6e2426e3c93fbd0
Sardar <sardar.andimeh7@gmail.com>
2024-07-16 18:51:22 +0200

    feat: check if key exists

--- insert-data/insert.go
+++ insert-data/insert.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/SardarAndimeh/ev101/db"
+	"github.com/go-redis/redis/v8"
 )
 
 func AddSimCards(filePath string) error {
@@ -23,6 +24,10 @@ func AddSimCards(filePath string) error {
 		msisdn := record[0]
 		key := "msisdn:" + msisdn
 		shardIndex := getShard(msisdn, totalRedis) // chose Redis Client according to Shard index (last 2 digits of MSISDN)
+
+		if checkKeyExists(db.Clients[shardIndex], key) {
+			continue
+		}
 
 		// create a map of key and values for each Sim card record ->  "MSISDN": 811502214250 , "IMSI": 217500013105250 ,....
 		recordMap := make(map[string]interface{})
@@ -47,4 +52,16 @@ func getShard(msisdn string, totalShards int) int {
 	lastDigit := msisdn[len(msisdn)-1]
 	shard := int(lastDigit-'0') % totalShards
 	return shard
+}
+
+func checkKeyExists(client *redis.Client, key string) bool {
+
+	exists, err := client.Exists(db.Ctx, key).Result()
+	if err != nil {
+		log.Fatalf("failed to check if key exists ")
+	}
+	if exists > 0 {
+		return true
+	}
+	return false
 }
