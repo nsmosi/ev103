6a57e3b2795a8830e930f6d0f0e2342918e5554f
Sardar <sardar.andimeh7@gmail.com>
2024-07-16 10:29:10 +0200

    parsing cmd arguments

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"flag"
+	"log"
+)
+
 func main() {
+	// parsing commad line arguments
+	configPath := flag.String("config", "", "path to the config file")
+	flag.Parse()
+
+	if *configPath == "" {
+		log.Fatalf("no arguments is provided , usage : go run main.go -config  path to config file")
+	}
 
 }
