53847ca123a93a89d12fe7cc34f7d935ca720d36
Sardar <sardar.andimeh7@gmail.com>
2024-07-16 20:28:04 +0200

    add search logic to getsimdata func

--- sim-query-api/handler.go
+++ sim-query-api/handler.go
@@ -1,15 +1,50 @@
 package simqueryapi
 
 import (
+	"log"
 	"net/http"
-	"strconv"
 
+	"github.com/SardarAndimeh/ev101/db"
 	"github.com/gin-gonic/gin"
 )
 
+type SimCard struct {
+	Msisdn int64  `json:"msisdn"`
+	Imsi   int64  `json:"imsi"`
+	Iccid  int64  `json:"iccid"`
+	Secret string `json:"secret"`
+	Tac    int    `json:"tac"`
+	Eid    int    `json:"eid"`
+	Cid    int    `json:"cid"`
+	Imei   int64  `json:"imei"`
+	Bundle struct {
+		ID       int    `json:"id"`
+		Ul       int    `json:"ul"`
+		Dl       int    `json:"dl"`
+		Quota    int64  `json:"quota"`
+		Duration int    `json:"duration"`
+		Label    string `json:"label"`
+		Type     string `json:"type"`
+	} `json:"bundle"`
+}
+
 func getSimData(context *gin.Context) {
 
-	msisdn, _ := strconv.ParseInt(context.Param("msisdn"), 10, 64)
+	msisdn := context.Param("msisdn")
+	key := "msisdn:" + msisdn
 
-	context.JSON(http.StatusOK, gin.H{"message": "test was successful", "msisdn": msisdn})
+	var record map[string]string
+	var err error
+
+	for _, client := range db.Clients {
+		record, err = client.HGetAll(db.Ctx, key).Result()
+		if err != nil {
+			log.Fatalf("failed to fetch data from db")
+		}
+		if len(record) > 0 {
+			context.JSON(http.StatusOK, gin.H{"found the sim card": record})
+			break
+		}
+	}
+
 }
